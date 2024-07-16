e56e8205511b43348b624443b6a9fdb33f17f0b4
o01071767279@nate.com <o01071767279@nate.com>
2024-07-16 21:51:07 +0900

    스프링 도입

--- src/main/java/tobyspring/springreview/Client.java
+++ src/main/java/tobyspring/springreview/Client.java
@@ -1,11 +1,15 @@
 package tobyspring.springreview;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.io.IOException;
 import java.math.BigDecimal;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        PaymentService paymentService = new PaymentService(new WebApiExRateProvider());
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
+        PaymentService paymentService = beanFactory.getBean(PaymentService.class);
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.valueOf(59.6));
         System.out.println("payment = " + payment);
     }
